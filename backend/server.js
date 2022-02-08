866b3665bf58f5e0b176715581d278c7f6e909b4
Dursun Selin <dursunseline@gmail.com>
2022-02-08 17:27:08 +0100

    Initialisation du frontend / Changement port backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const normalizePort = val => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
 // Fonction pour recherche de différentes erreurs et gestion des erreurs
