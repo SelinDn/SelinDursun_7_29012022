421eac40e52bd9153731f6d7868ad6c5d2d877ad
Dursun Selin <dursunseline@gmail.com>
2022-02-18 19:34:39 +0100

    Récupération des commentaires + logique

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,10 +2,13 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Upload from "../../components/Post/Upload";
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import CommentIcon from '@material-ui/icons/Comment';
 import "../Home/Home.css";
 
 function Home() {
     const [posts, setPosts] = useState([]);
+    const [comment, setComment] = useState("");
+    const [comments, setComments] = useState([]);
     
     useEffect(() => {
         const token = localStorage.getItem("Token")
@@ -28,8 +31,14 @@ function Home() {
             <h1 className="home-title">Récentes publications</h1>
             {posts.map((post) => {
 
+                /**
+                 * Logique likePost,
+                 * handleSubmitComment pour création de commentaires,
+                 * getComments pour récupération de tout les commentaires,
+                 * (Tous rattachés au useEffect de getAllPosts).
+                 */
                 const likePost = () => {
-                    const token = localStorage.getItem("Token")
+                    const token = localStorage.getItem("Token");
                     axios({
                         method: "post",
                         url: `http://localhost:3001/api/posts/${post.id}`,
@@ -43,9 +52,47 @@ function Home() {
                     })
                     .then((res) => {
                         setPosts(res.data);
+                        window.location.reload();
                     })
                     .catch((error) => console.log(error));
-                }
+                };
+
+                const handleSubmitComment = () => {
+                    const token = localStorage.getItem("Token");
+                    axios({
+                        method: "post",
+                        url: `http://localhost:3001/api/posts/${post.id}/comments`,
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                        data: {
+                            content: comment,
+                            postId: post.id,
+                            userId: token.userId,
+                        },
+                    })
+                    .then((res) => {
+                        setPosts(res.data);
+                        window.location.reload();
+                    })
+                    .catch((error) => console.log(error));
+                };
+
+                const getComments = () => {
+                    const token = localStorage.getItem("Token");
+                    axios({
+                        method: "get",
+                        url: `http://localhost:3001/api/posts/${post.id}/comments`,
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    })
+                    .then((res) => {
+                        setPosts(res.data);
+                        setComments(res.data);
+                    })
+                    .catch((error) => console.log(error));
+                };
 
                 return (
                     <div className="post" key={post.id}>
@@ -68,8 +115,41 @@ function Home() {
                                 id="like-btn" 
                                 onClick={likePost} 
                             />
-                            Aimé par {post.like} personnes
+                            Aimé par {post.like.length} personnes
+                            <CommentIcon 
+                                id="comment-btn" 
+                                onClick={getComments} 
+                            />
                         </div>
+                        <form className="comment-form" action="" onSubmit={handleSubmitComment}>
+                            <input 
+                                type="textarea" 
+                                name="comment" 
+                                id="comment" 
+                                onChange={(e) => setComment(e.target.value)} 
+                                value={comment}
+                                placeholder="Say hello to..." 
+                            />
+                            <br />
+                            <input type="submit" value="Publier" className="btn-comment" />
+                        </form>
+                        {comments.map((comment) => {
+                            return (
+                                <div className="comments-container" key={post.id}>
+                                    <div className="post-comments">
+                                        <div className="post-comments-header">
+                                            Posté par {comment.userId}
+                                        </div>
+                                        <div className="post-comments-date">
+                                            {new Date(comment.createdAt).toLocaleDateString("fr-FR")}
+                                        </div>
+                                    </div>
+                                    <div className="post-comments-content">
+                                        {comment.content}
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 )
             })}
